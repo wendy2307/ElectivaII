54b4fc6756fa5709863aaa2e05399382e1fc7092
wendy2307 <126276808+wendy2307@users.noreply.github.com>
2024-04-21 13:15:07 -0500

    Delete Main.js

--- Main.js
+++
@@ -1,9 +0,0 @@
-export const crearmain = (relleno)=>{
-    const main = document.createElement('main');
-    
-    const mensajeBienvenida = document.createElement('p');
-    mensajeBienvenida.textContent = relleno ;
-    main.appendChild(mensajeBienvenida);
-    
-    return main;
-    };
